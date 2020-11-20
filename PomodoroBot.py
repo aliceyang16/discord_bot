07d7cafc7d9595770106198a808087afdbdf5c0e
Alice Yang <yangalice8@gmail.com>
2020-11-20 13:02:50 +0200

    set break timers

--- PomodoroBot.py
+++ PomodoroBot.py
@@ -16,12 +16,22 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='SetTimer', help='Sets Pomodoro timer in minutes')
 async def SetTimer(ctx, inputTime):
-	session.setTimer(int(inputTime))
+	session.setWorkTimer(int(inputTime))
 	await ctx.send('Pomodoro timer has been set for ' + inputTime + ' minutes')
+
+@bot.command(name='SetBreak', help='Set break timer in minutes for pomodoro session')
+async def SetBreak(ctx, inputTime):
+	session.setBreakTimer(int(inputTime))
+	await ctx.send("You've set a break time for " + inputTime + " minutes :smiley:")
 
 @bot.command(name='StartTimer', help='Starts the pomodoro timer')
 async def StartTimer(ctx):
-	message = session.startTimer()
+	message = session.startWorkTimer()
 	await ctx.send(message)
+	message = session.startBreakTimer()
+	await ctx.send(message)
+
+
+
 
 bot.run(TOKEN)
--- timer.py
+++ timer.py
@@ -3,20 +3,35 @@ import time
 
 class PomodoroTimer:
 	def __init__(self, time):
+		# default 25 minutes working session and 5 minutes break
+		self.time = 1500
+		self.breakTime = 300
+
+	def setWorkTimer(self, time):
 		self.time = time * 60
 
-	def setTimer(self, time):
-		self.time = time * 60
+	def setBreakTimer(self, time):
+		self.breakTime = time * 60
 
-	def startTimer(self):
-		while self.time:
-			mins, secs = divmod(self.time, 60)
+	def startWorkTimer(self):
+		self.startTimer(self.time)
+		message = "Good work! Hope you were productive in the last " + str(int(self.time / 60)) + " minutes! :grinning:"
+		return message
+
+	def startBreakTimer(self):
+		self.startTimer(self.breakTime)
+		message = "Hope you had a good " + str(int(self.breakTime / 60)) + " minutes break! :zany_face:"
+		return message
+
+	def startTimer(self, t):
+		while t:
+			mins, secs = divmod(t, 60)
 			timer = '{:02d}:{:02d}'.format(mins, secs)
 			print(timer, end='\r')
 			time.sleep(1)
-			self.time -= 1
+			t -= 1
 		print("ALL DONE")
-		return "TIME'S UP!"
+
 
 # session = PomodoroTimer(5)
 # session.startTimer()
