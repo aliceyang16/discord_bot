17e16d359e503b2d4f90da22aafde8158a572069
Alice Yang <yangalice8@gmail.com>
2020-12-05 14:39:35 +0200

    threading implementation

--- pomodoro_bot/PomodoroBot.py
+++ pomodoro_bot/PomodoroBot.py
@@ -1,73 +1,74 @@
 import os
 
-from timer import PomodoroTimer, BotTime
-
 from dotenv import load_dotenv
 from discord.ext import commands
+
+import threading_time
+from threading_time import FuncThread
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-session = PomodoroTimer()
+pomodoro_thread = FuncThread("Pomodoro Thread")
 bot = commands.Bot(command_prefix="!")
 
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user} is connected to the Discord!")
+	print(f"{bot.user} is connected to the Discord!")
 
 
-@bot.command(name="SetTimer", help="Sets Pomodoro timer in minutes")
+@bot.command(name="setTimer", help="Sets Pomodoro timer in minutes")
 @commands.has_role('Admin')
 async def SetTimer(ctx, inputTime):
-	if int(inputTime) >= 0 and int(inputTime) <= 40:
-	    session.setWorkTimer(int(inputTime))
-	    await ctx.send("Pomodoro timer has been set for " + inputTime + " minutes")
-	    timeout_session.resetTimer()
-	else:
-		await ctx.send("Invalid time value! :sob:")
+    if int(inputTime) >= 0 and int(inputTime) <= 40:
+        pomodoro_thread.setProductivityTime(int(inputTime))
+        await ctx.send("Pomodoro timer has been set for " + inputTime + " minutes")
+    else:
+        await ctx.send("Invalid time value! :sob:")
 
 
-@bot.command(name="SetBreak", help="Set break timer in minutes for pomodoro session")
+@bot.command(name="setBreak", help="Set break timer in minutes for pomodoro session")
 @commands.has_role('Admin')
 async def SetBreak(ctx, inputTime):
-    session.setBreakTimer(int(inputTime))
-    if int(inputTime) >= 0 and int(inputTime) <= 10:
-	    await ctx.send("You've set a break time for " + inputTime + " minutes :smiley:")
-	    timeout_session.resetTimer()
+	if int(inputTime) >= 0 and int(inputTime) <= 10:
+		pomodoro_thread.setBreakTime(int(inputTime))
+		await ctx.send("You've set a break time for " + inputTime + " minutes :smiley:")
+		timeout_session.resetTimer()
 	else:
 		await ctx.send("Invalid time value! :sob:")
 
-@bot.command(name="Check", help="Check the times set for pomodoro sessions")
+@bot.command(name="check", help="Check the times set for pomodoro sessions")
 async def Check(ctx):
-    working_time = str(int(session.time / 60))
-    break_time = str(int(session.breakTime / 60))
-    await ctx.send("Productivity Time: " + working_time + "minutes \n" + "Break Time: " + break_time + " minutes")
+	working_time = str(int(pomodoro_thread.productivity / 60))
+	break_time = str(int(pomodoro_thread.breakSession / 60))
+	await ctx.send("Productivity Time: " + working_time + "minutes \n" + "Break Time: " + break_time + " minutes")
 
-@bot.command(name="StartTimer", help="Starts the pomodoro timer")
+@bot.command(name="start", help="Starts the pomodoro timer")
 @commands.has_role('Admin')
-async def StartTimer(ctx):
-    message = session.startWorkTimer()
-    await ctx.send(message)
-    message = session.startBreakTimer()
-    await ctx.send(message)
-    timeout_session.resetTimer()
+async def runTimer(ctx):
+	await ctx.send("Starting Productivity Session :smiley:")
+	message = pomodoro_thread.runProductivity()
+	await ctx.send(message)
+	await ctx.send("Starting well-deserved break session!")
+	message = pomodoro_thread.runBreak()
+	await ctx.send(message)
+
+@bot.command(name="stop", help="Stops the current timer.")
+async def stopTimer(ctx):
+	exit_flag = True
+	await ctx.send("Timer has been stopped")
 
 
 @bot.command(name="exit", help="Logs pomodoro bot out of discord")
 @commands.has_role('Admin')
 async def botExit(ctx):
-    await ctx.send("Farewell, everyone! :hugging: :hugging_face:")
-    await bot.close()
+	await ctx.send("Farewell, everyone! :hugging: :hugging_face:")
+	await bot.close()
 
 @bot.event
 async def on_command_error(ctx, error):
-    if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send('You do not have the correct role for this command.')
-
-#if timeout_session.timeout == -1:
-#    bot.close()
-#else:
-#    bot.run(TOKEN)
+	if isinstance(error, commands.errors.CheckFailure):
+		await ctx.send('You do not have the correct role for this command.')
 
 bot.run(TOKEN)
---
+++ pomodoro_bot/threading_time.py
@@ -0,0 +1,50 @@
+# Threading and Timer
+import threading
+import time
+
+exit_flag = False
+
+def runTimer(thread, counter: int):
+	while counter:
+		if exit_flag:
+			break
+		time.sleep(1)
+		print("%s: %s" %(thread.name, time.ctime(time.time())))
+		counter -= 1
+
+class FuncThread (threading.Thread):
+	def __init__(self, name: str):
+		threading.Thread.__init__(self)
+		self.name = name
+		self.productivity = 1500
+		self.breakSession = 300
+
+	def setProductivityTime(self, productivity: int) -> None:
+		self.productivity = productivity * 60
+
+	def setBreakTime(self, breakTime: int) -> None:
+		self.breakSession = breakTime * 60
+
+	def runProductivity(self) -> str:
+		print("Running productivity session for " + self.name)
+		runTimer(self, self.productivity)
+		print("Exiting productivity session for " + self.name)
+		return "Good work! Hope you were productive in the last " + str(int(self.productivity / 60)) + " minutes! :grinning:"
+	
+	def runBreak(self):
+		print("Running break session for " + self.name)
+		runTimer(self, self.breakSession)
+		print("Exiting break session for " + self.name)
+		return "Hope you had a good " + str(int(self.breakSession / 60)) + " minutes break! :zany_face:"
+
+	def run(self):
+		self.runProductivity()
+		self.runBreak()
+
+# -------- testing --------------
+# thread1 = FuncThread("thread1")
+# thread1.setProductivityTime(5)
+# thread1.setBreakTime(2)
+# thread1.start()
+# time.sleep(2)
+# exit_flag = True
